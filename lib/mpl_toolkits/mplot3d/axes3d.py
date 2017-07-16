58b931101d0e4b636defcb93dfd8acf25434c828
Eric Wieser <wieser.eric@gmail.com>
2017-07-16 17:32:34 +0100

    ENH: Set the default plotbox aspect to match how renders looked previously

--- lib/mpl_toolkits/mplot3d/axes3d.py
+++ lib/mpl_toolkits/mplot3d/axes3d.py
@@ -999,6 +999,8 @@ class Axes3D(Axes):
         point.
 
         """
+        pb_aspect = np.array([1, 1, 0.75])
+
         relev, razim = np.pi * self.elev/180, np.pi * self.azim/180
 
         xmin, xmax = self.get_xlim3d()
@@ -1008,10 +1010,10 @@ class Axes3D(Axes):
         # transform to uniform world coordinates 0-1.0,0-1.0,0-1.0
         worldM = proj3d.world_transformation(xmin, xmax,
                                              ymin, ymax,
-                                             zmin, zmax)
+                                             zmin, zmax, pb_aspect=pb_aspect)
 
         # look into the middle of the new coordinates
-        R = np.array([0.5, 0.5, 0.5])
+        R = pb_aspect / 2
 
         xp = R[0] + np.cos(razim) * np.cos(relev) * self.dist
         yp = R[1] + np.sin(razim) * np.cos(relev) * self.dist
